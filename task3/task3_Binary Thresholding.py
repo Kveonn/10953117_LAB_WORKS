8ef551a53d9a47392fa6ed8e089406112ad7f509
Kveonn <99091826+Kveonn@users.noreply.github.com>
2025-08-03 10:45:53 -0700

    Update task3_Binary Thresholding.py
    Explanation:
    Pixel value: 180
    Threshold: 150
    Since 180 > 150, the pixel value is above the threshold
    Therefore, the new pixel value = 255
    
    Binary Thresholding Rule:
    If pixel value > threshold → output = 255
    If pixel value ≤ threshold → output = 0

--- task3/task3_Binary Thresholding.py
+++ task3/task3_Binary Thresholding.py
@@ -1,11 +1,12 @@
 """
+NAME: SAMUEL OSEI BOAKYE
 ID:10953117
 COURSE: COMPUTER VISION
 """
 import cv2
 import numpy as np
 
-# Single pixel example
+# Single pixel 
 pixel = np.array([[180]], dtype=np.uint8)
 threshold_value = 150
 _, binary = cv2.threshold(pixel, threshold_value, 255, cv2.THRESH_BINARY)
@@ -13,4 +14,4 @@ _, binary = cv2.threshold(pixel, threshold_value, 255, cv2.THRESH_BINARY)
 print(f"Original: {pixel[0][0]}")
 print(f"Thresholded: {binary[0][0]}") 
 
-# Will output 255
+# The codes Will output 255
